5578c487df6f55d8dd73f761f09219048a86cbd2
arvind <an439@njit.edu>
2018-09-25 21:17:42 -0400

    inital commit

---
+++ src/App.js
@@ -0,0 +1,15 @@
+import React, { Component } from 'react';
+import Projects from './Components/projects';
+
+class App extends Component {
+  render() {
+    return (
+      <div className="App">
+  My App
+  <Projects />
+      </div>
+    );
+  }
+}
+
+export default App;
---
+++ src/Components/projects.js
@@ -0,0 +1,14 @@
+import React, { Component } from 'react';
+
+
+class projects extends Component {
+  render() {
+    return (
+      <div className="projects">
+  my projects
+      </div>
+    );
+  }
+}
+
+export default projects;
